cee914092f426880f0087d75faf37c533bc7d457
arikiana <167075016+arikiana@users.noreply.github.com>
2025-04-01 15:46:38 +0200

    Update and rename Welcomepage.py to Welcome Page.py

--- Welcomepage.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-st.title("Nice to see you! Ready to be mAItched?")
-st.write("Welcome to mAItcher, your connective AI-platform.")
-st.header("How it works:")
-st.write("Instead of typing your prompt into every different AI-chatbot you know and then deciding which answer is best, you can just give your prompt to me. I will mAItch you with the best fit model for your request."
-st.page_link("pages/Promptinginterface.py", label = "Get started", icon = "💫")
-
-  
