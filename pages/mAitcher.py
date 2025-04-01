6c7941fd42a1f71a11458a949f0fb739b5d57683
l-rose-lab <lilymappin@outlook.com>
2025-04-01 17:47:22 +0200

    Add chat bubble styling to the user prompt

--- pages/mAitcher.py
+++ pages/mAitcher.py
@@ -3,4 +3,9 @@ import streamlit as st
 st.title("mAItcher")
 prompt = st.chat_input("Write your prompt here")
 if prompt:
-    st.write(f"You: {prompt}")
+    chat_box = f"""
+    <div style='border 1px solid black; padding: 10px; border-radius: 8px; background-color: #f0f0f0; margin: 10px;'>
+    You: {prompt}
+    <div/>
+    
+    st.markdown (chat_box, unsafe_allow_html=True)
