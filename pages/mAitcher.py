3d5a9c2657b2719ae17bc0b92914a908af26f954
l-rose-lab <lilymappin@outlook.com>
2025-04-01 18:39:06 +0200

    Update mAitcher.py

--- pages/mAitcher.py
+++ pages/mAitcher.py
@@ -2,10 +2,23 @@ import streamlit as st
 
 st.title("mAItcher")
 prompt = st.chat_input("Write your prompt here")
+
 if prompt:
     chat_box = f"""
-    <div style='border 1px solid black; padding: 10px; border-radius: 8px; background-color: #f0f0f0; margin: 10px;'>
-    You: {prompt}
-    <div/>
+    <div style='
+        border 1px solid black; #HTML to create bubble like chat style, used ChatGPT to explain, navigate and correct code.
+        padding: 10px;
+        border-radius: 8px;
+        background-color: #f0f0f0;
+        margin: 10px;
+        max-width: 80%
+        width: fit-content
+    '>
+
+         You: {prompt}
+    </div>
+    """
+    st.markdown (chat_box, unsafe_allow_html=True)
+
     
     st.markdown (chat_box, unsafe_allow_html=True)
