b5877c4fc22da6ae027532fd2f18d8310f91b8c8
arikiana <167075016+arikiana@users.noreply.github.com>
2025-04-01 15:27:58 +0200

    Update and rename Welcomepage.py to pages/Welcomepage.py

--- pages/Welcomepage.py
+++ pages/Welcomepage.py
@@ -3,6 +3,6 @@ st.title("Nice to see you! Ready to be mAItched?")
 st.write("Welcome to mAItcher, your connective AI-platform.")
 st.header("How it works:")
 st.write("Instead of typing your prompt into every different AI-chatbot you know and then deciding which answer is best, you can just give your prompt to me. I will mAItch you with the best fit model for your request."
-st.page_link("mAitcher/Promptinginterface.py", label = "Get started", icon = "💫")
+st.page_link("pages/Promptinginterface.py", label = "Get started", icon = "💫")
 
   
