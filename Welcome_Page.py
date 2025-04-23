9280c9c30fdd95ac7b4ef6495b9b2da012791af1
arikiana <167075016+arikiana@users.noreply.github.com>
2025-04-23 16:25:39 +0200

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -1,13 +1,14 @@
+# Startseite für die App, welche die Funktion für User:innen erklärt
 import streamlit as st
 
 st.title("Nice to see you! Ready to be mAitched?") 
 st.markdown('''
-    :rainbow[Welcome to mAItcher, your connective AI-platform.]''') #raibow markdown creates a colorful effect in the text, code sourced through streamlit discussion forum
+    :rainbow[Welcome to mAItcher, your connective AI-platform.]''') #färbt den Text regenbogenfarbig für Ästethische Zwecke, Idee zum Code wurde der streamlit documentation entnommen
 st.markdown('''
-    :blue[**How it works:**] ''') #blue markdown for blue text, code sourced through streamlit discussion forum
+    :blue[**How it works:**] ''') #färbt den Text blau
 
 st.markdown('''
     Instead of typing your prompt into every different AI-chatbot you know and then deciding which answer is best, you can just give your prompt to me. I will mAItch you with the 
-    :blue-background[best fit model] for your request.''') #hilights best fit in blue, code sourced through streamlit discussion forum
+    :blue-background[best fit model] for your request.''') #hebt den wichtigsten Teil der Aussage mit blauem Hintergrund hervor
 
-st.page_link("pages/mAitcher.py", label = "Get started", icon = "💫") #creates a Button to the interaction page with maitcher
+st.page_link("pages/mAitcher.py", label = "Get started", icon = "💫") #Button zur Interaktonsseite mit mAItcher
