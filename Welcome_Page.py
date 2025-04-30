bbadab79f7fd39d1f943b2c6ee17338f8bd9073b
arikiana <167075016+arikiana@users.noreply.github.com>
2025-04-30 12:35:08 +0200

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -1,5 +1,6 @@
 # Startseite für die App, welche die Funktion für User:innen erklärt
 import streamlit as st
+import numpy as np
 
 st.title("Nice to see you! Ready to be mAitched?")
 
@@ -13,4 +14,17 @@ st.markdown('''
     Instead of typing your prompt into every different AI-chatbot you know and then deciding which answer is best, you can just give your prompt to me. I will mAItch you with the 
     :orange-background[:violet[best fit model]] for your request.''') #hebt den wichtigsten Teil der Aussage mit orangenem Hintergrund und lila Schrift hervor
 
-st.page_link("pages/mAitcher.py", label = "Get started", icon = "💫") #Button zur Interaktonsseite mit mAItcher, aus Streamlit Dokumentation
+st.page_link("pages/mAitcher.py", label = "Click here to get started", icon = "💫") #Button zur Interaktonsseite mit mAItcher, aus Streamlit Dokumentation
+
+tab1, tab2 = st.tabs(["User Frequency", "Visit other AI-Models"]) #Tabs um die Visualisierung zu zeigen
+
+tab1.subheader("Most popular AI-models")
+tab1.text('We have tracked the most frequent matches between what our users search for and the different AIs. All relevant information can be found in the chart below:')
+
+#Links um direkt auf die AIs zugreifen zu können, Funktionen via Streamlit Documentation 
+tab2.subheader("Links")
+tab2._link_button('ChatGPT', 'https://chatgpt.com/', "primary", icon="🌐")
+tab2._link_button('Gemini', 'https://gemini.google.com/', "primary", icon="🚀")
+tab2._link_button('Mistral', 'https://mistral.ai/', "primary", icon="🐯")
+tab2._link_button('Grok','https://grok.com/', "primary", icon="🪐")
+tab2._link_button('Claude', 'https://claude.ai/', "primary", icon="👾")
