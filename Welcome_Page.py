ce21d237196649ef7336552d1795b5a7f19b28f2
arikiana <167075016+arikiana@users.noreply.github.com>
2025-04-30 20:51:30 +0200

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -1,3 +1,6 @@
+import streamlit as st
+st.write("🔒 Loaded secrets:", dict(st.secrets))
+
 # Startseite für die App, welche die Funktion für User:innen erklärt
 import streamlit as st
 import numpy as np
