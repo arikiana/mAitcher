a3aa8559961bc0e93ff3ad9823087691fcb03765
arikiana <167075016+arikiana@users.noreply.github.com>
2025-05-03 11:00:43 +0200

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -1,4 +1,4 @@
-# Used ChatGPT to help guide the writing and correction of the following code
+# ChatGPT wurde zur Korrektur & dem debugging des folgenden Codes verwendet
 
 
 import streamlit as st
@@ -10,14 +10,14 @@ st.set_page_config(
 )
 
 
-st.title("Nice to see you! Ready to be mAitched?")
+st.title("Nice to see you! Ready to be mAitched?") #Funktion aus der Streamlit Documentation zur Hervorhebung des Titels
 
 
-tab1, tab2 = st.tabs(["Welcome to mAItcher", "Explore AI Models"])
+tab1, tab2 = st.tabs(["Welcome to mAItcher", "Explore AI Models"]) #zur Übersichtlichkeit wird die Homepage auf zwei Tabs aufgeteilt, Funktion aus Streamlit Documentation
 
-
+# mit markdown wurde die Farbe der Texte sowie deren Format weiter verändert, um die Lesbarkeit des Textes zu steigern und wichtige Elemente hervorzuheben (aus Streamlit Documentation)
 with tab1:
-    st.markdown(":rainbow[**Welcome to mAItcher, your connective AI-platform.**]")
+    st.markdown(":rainbow[**Welcome to mAItcher, your connective AI-platform.**]") 
     st.markdown(
         ":violet[**How it works:**]  "
         "Instead of typing your prompt into every different AI-chatbot and then deciding which answer is best, "
@@ -26,13 +26,13 @@ with tab1:
     )
     st.page_link(
         "pages/mAitcher.py",
-        label="💫 Click here to get started",
+        label="Click here to get started",
         icon="💫"
-    )
+    ) #button, der einen direkt mit dem chatbot verbindet
 
-
+# um bei einer Präferenz für einzelne Ai-Platformen direkt darauf zugreifen zu können, werden die verwendeten Modelle separat verlinkt
 with tab2:
-    st.subheader("Links to AI Platforms")
+    st.markdown(":rainbow[**Links to AI Platforms**]")
     st.markdown("""
     - 🌐 [ChatGPT](https://chatgpt.com/)  
     - 🚀 [Gemini](https://gemini.google.com/)  
